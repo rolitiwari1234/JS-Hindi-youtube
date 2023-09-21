3b2b564374de6443105cd72c19a33a282e2978ea
rolitiwari1234 <145473025+rolitiwari1234@users.noreply.github.com>
2023-09-21 16:21:18 +0000

    container config  done

--- .devcontainer/01_basics/conversionoperation.js
+++ .devcontainer/01_basics/conversionoperation.js
@@ -22,8 +22,46 @@ let score = "roli"
 
   let someNumber = 44
 
-  let stringNumber = string(someNumber)
-  console.log(stringNumber);
-  console.log(typeof stringNumber);
+  let StringNumber = String(someNumber)
+  // console.log(StringNumber);
+  // console.log(typeof StringNumber);
+
+  // **************operation***************  
+
+  let Value = 3
+  let negValue = -Value
+  // console.log(negValue);
+
+  // console.log(2+2);
+  // console.log (2-2);
+  // console.log(2*3);
+  // console.log(2**4);
+  // console.log(9/3);
+  // console.log(3%8);
+
+  let str1 = "hello"
+  let str2 = " roli"
+
+  let str3 = str1+str2
+  console.log(str3);
+
+  console.log( 1 + "2" )
+  console.log ("1" + 2 )
+  console.log ( "1"+ 2 + 2)
+  console.log (1 + 2 + "2")
+
+  // console.log (( 2 + 4) * 3 % 2 )
+
+  console.log (+true);
+  console.log (+"");
+
+  let num1, num2, num3
+
+  num1 = num2 = num3 = 2 + 2
+
+  let gameCounter = 100
+  gameCounter++ ;
+  console.log(gameCounter);
+
 
 
