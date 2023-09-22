fcd467de4189408ee34cb4248671afd2d3d96326
rolitiwari1234 <145473025+rolitiwari1234@users.noreply.github.com>
2023-09-22 10:52:50 +0000

    container config done

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//  primitive
+
+// 7 type = string, number, undefined, null, boolean, symbol, BigInt
+
+const Score = 100
+const Scorevalue = 100.4
+
+const inLoggedIn = false
+const outTemp = null
+let useEmail ;
+
+const id = Symbol ('123')
+const anotherId = Symbol ( '123')
+
+// console.log ( id === anotherId);
+
+// const bigNumber = 12275896575788n
+
+
+// Reference
+
+// Array, objects, Functions 
+
+const Heros = ["Shaktiman","Naagraj","Doga"]
+let myObj = {
+    name : "roli",
+    age :18 ,
+}
+
+const myFunction = Function() 
+{
+  console.log ("hello world") ;   
+} 
+console.log (typeof anotherId);
+
+
