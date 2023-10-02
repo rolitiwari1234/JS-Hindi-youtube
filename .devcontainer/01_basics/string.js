f6d14cd67bbd25c56188f49f21dccb4b8e942aca
rolitiwari1234 <145473025+rolitiwari1234@users.noreply.github.com>
2023-10-02 15:03:33 +0000

    devcontainer config done

--- .devcontainer/01_basics/string.js
+++ .devcontainer/01_basics/string.js
@@ -3,4 +3,30 @@ const reopCount = 100
 
 // console.log (name + reopCount + " value"); donot use
 
-console.log(`my name is ${name} and repo count is${reopCount}`)
+// console.log(`my name is ${name} and repo count is${reopCount}`)
+
+const gameName = new String ('roli-gr-gs')
+// console .log (gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt('3') );
+// console.log(gameName.indexOf('o'));
+
+const newString = gameName.substring(0,3)
+// console.log(newString);
+const anotherstring = gameName.slice(-4,4)
+// console.log(anotherstring);
+
+const newstringOne = "  roli  "
+// console.log(newstringOne);
+// console.log(newstringOne .trim());
+
+const url = "https://roli.com/roli30%tiwari"
+ console.log(url.replace('30%' ,'-'));
+
+ console.log(url.includes('surya'));
+
+ console .log(gameName.split('-'))
