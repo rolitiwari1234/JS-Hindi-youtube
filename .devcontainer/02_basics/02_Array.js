57ffeb22cc97041dba13eee6963077aec9757f57
rolitiwari1234 <145473025+rolitiwari1234@users.noreply.github.com>
2023-10-05 01:13:50 +0000

    02_basics

---
+++ .devcontainer/02_basics/02_Array.js
@@ -0,0 +1,27 @@
+const marval_heros = ["spiderman" ,"thor","ironman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marval_heros.push(dc_heros)
+
+// console.log(marval_heros);
+// console.log(marval_heros[ 3 ] [ 1 ]);
+
+// const AllHeros = marval_heros.concat(dc_heros)
+// console.log(AllHeros);
+
+const all_new_heros = [ ...marval_heros, ...dc_heros ]
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3 ,[4, 5, 6] , 7,[6, 7,[4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+
+console.log(Array.isArray ("Yashi"))
+console.log(Array.from("Yashi"))
+ console.log(Array.from({name : "Yashi" })) //intersting
+
+ let score1 = 100
+ let score2 = 200
+ let score3 = 300
+
+ console.log(Array.of(score1, score2, score3))
